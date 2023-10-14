98a4e2ee368efce8b2b71b124f7157ed8a8eca0f
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-14 14:11:16 +0200

    Update e_even1-20.mjs

--- e_even1-20.mjs
+++ e_even1-20.mjs
@@ -2,4 +2,10 @@ import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 const userInput = readline.createInterface({ input, output });
 
-for (let i = 1; i <= 20; i++)
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+process.exit();

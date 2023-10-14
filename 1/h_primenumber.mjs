b52e9f5078263a3ffcdbba0bfd8ffebe362b007a
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-14 15:03:06 +0200

    update main

---
+++ 1/h_primenumber.mjs
@@ -0,0 +1,31 @@
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+const userInput = readline.createInterface({ input, output });
+
+let n = parseFloat(await userInput.question('Geef een getal: '));
+
+function isPrime(number) {
+    if (number < 2) {
+        return false;
+    } else {
+        for (let i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        } return true;
+    }
+}
+
+function printPrime(n) {
+    for (let i = 2; i <= n; i++) {
+        if (isPrime(i)) {
+            console.log(i);
+        }
+    }
+}
+
+printPrime(n);
+//console.log(isPrime(n));
+
+
+process.exit();
---
+++ 2/e_som.mjs
@@ -0,0 +1,18 @@
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+const userInput = readline.createInterface({ input, output });
+
+let getal = parseFloat(await userInput.question('Geef een getal: '));
+let getal2 = parseFloat(await userInput.question('Geef een tweede getal: '));
+let getal3 = parseFloat(await userInput.question('Geef een derde getal: '));
+let getal4 = parseFloat(await userInput.question('Geef een vierde getal: '));
+let som = 0;
+let numbers = [getal, getal2, getal3, getal4];
+
+for (let i = 0; i < numbers.length; i++) {
+    som += numbers[i];
+}
+
+console.log('De som van de getallen is: ' + som);
+
+process.exit();

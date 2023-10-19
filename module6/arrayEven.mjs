2e207139db62c6d27095bffcb75219aa744a0beb
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-10-19 19:05:52 +0200

    update main

--- module6/arrayEven.mjs
+++ module6/arrayEven.mjs
@@ -5,14 +5,20 @@ const userInput = readline.createInterface({input, output});
 let getal1 = parseFloat(await userInput.question('Geef getal 1: '));
 let getal2 = parseFloat(await userInput.question('Geef getal 2: '));
 let getal3 = parseFloat(await userInput.question('Geef getal 3: '));
+let getal4 = parseFloat(await userInput.question('Geef getal 4: '));
+let getal5 = parseFloat(await userInput.question('Geef getal 5: '));
 
 let getallen = [getal1, getal2, getal3];
 
-function arrayMax (getallen) {
-    let max = Math.max(...getallen);
-    return max;
+function arrayEven (getallen) {
+    let even = [];
+    for (let i = 0; i < getallen.length; i++) {
+        if (getallen[i] % 2 == 0)
+        even.push(getallen[i]);
+    }
+    return even;
 }
 
-console.log(arrayMax(getallen));
+console.log(arrayEven(getallen));
 
 process.exit();
